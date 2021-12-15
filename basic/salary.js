01f4f25deaa05e130ad84d436685e9f7e0c4f21f
unknown <victoria.prtj@gmail.com>
2021-12-15 12:56:05 -0300

    todas js

---
+++ basic/salary.js
@@ -0,0 +1,5 @@
+let seller = joao
+let fixsal = 500
+let sale = 1230.30
+let bonus = sale*0.15
+let finalsal = fixsal+bonus
---
+++ hobby/hobby.js
@@ -0,0 +1,14 @@
+document.getElementById('header').innerHTML = 'RPGista por diversão, escritora por paixão';
+
+document.getElementById('leftbox').src = "photo.jpg";
+
+document.getElementById('textbox').innerHTML = 'Sendo uma grande fã de épicos de fantasia, e uma àvida jogadora de RPG, uma das minhas tarefas preferidas é imaginar mundos e realizar o chamado "WorldBuilding", pensando estrutura social, geografia, cultura e personagens locais. Dentro dessa vontade, me aventurei a escrever, não apenas informações gerais sobre o mundo, mas parte de uma narratia em si. Buscando um refúgio pessoal e uma forma de me expressar artisticamente, o que me ocorreu foi de me aventurar como escritora, e escrever uma narrativa dentro de um mundo de fantasia autoral, com tema magipunk (onde tecnologia é energizada a partir de energia mágica). Eis um excerto de minha narrativa: "Uma lanterna luminescente num tom azulado iluminava a cabine que dançava junto das ondas do mar, enquanto Elizabeth terminava as últimas anotações em seu diário. A moça de pele negra e cabelos cacheados trajava roupas de pano de botas de couro, olhava pela janela à esquerda do navio à vapor, enquanto fechava seu caderno e o guardava em sua grande bolsa de couro escurecido. A seu lado, um sabre de cabo prateado se apoiava na cabeceira da cama, e em cima da escrivaninha corroída pela maresia, repousava um revólver ornamentado, com cartuchos de arcanita empilhados na mesa. A moça retirou um cachimbo bem trabalhado de madeira faraniana e o abasteceu com erva cerrilhada, retirou-se de seu pequeno aposento e escorou-se em uma das laterais do navio, que cultivavam uma vista que abrangia o grande mar aberto que se encontrava com o horizonte. Sentindo os ventos salgados do mar, retirou um isqueiro velho do bolso e acendeu o restante do tabaco que repousava em seu cachimbo, expirando a fumaça logo em seguida, "esse é último trabalho que faço para esse bando de imbecis ingratos", pensou consigo mesma enquanto preparava seus pulmões para o próximo trago, mas dessa vez, o odor opulento do combustível à base de arcanita e óleo marinho invadiu suas narinas, "os goblins são criaturas extraordinariamente inteligentes, capazes de criarem maravilhas tecnológicas como esse extraordinário navio, mas ainda assim não conseguem resolver esse cheiro fétido do combustível. Esse é o preço que pagam por viverem onde vivem", e aquietando seus pensamentos, exalou a fumaça do segundo trago que se dissipou rapidamente com os ventos da Corrente de Petulon." ';
+
+document.getElementById('name').innerHTML = "Nome: Victoria Maria de Queiroz Lopes de Abreu";
+
+document.getElementById('age').innerHTML = "Idade: 25";
+
+document.getElementById('state').innerHTML = "Estado: São Paulo";
+
+document.getElementById('footer').innerHTML = "@Victoria Maria";
+
